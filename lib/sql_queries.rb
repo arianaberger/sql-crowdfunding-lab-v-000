775758722464ca8c5ccc88882baf6d64ff73fb67
Ariana Berger <ariana.berger@gmail.com>
2018-03-14 20:31:58 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
-WHERE projects.category = 'music'
+WHERE projects.category = 'music' #this line isn't working
 ORDER BY projects.category"
 end
 
