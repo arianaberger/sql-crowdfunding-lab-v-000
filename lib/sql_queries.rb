f0b172f561b0fa946aa8e3ab0586ca9590ceb034
Ariana Berger <ariana.berger@gmail.com>
2018-03-14 20:44:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,12 +32,12 @@ HAVING goal_amount >= 0" #I want to substract the total funding - goal and see i
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, sum(pledges.amount)
+"SELECT users.name, sum(pledges.amount) AS total_pledge
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
 GROUP BY users.name
-ORDER BY pledges.amount" 
+ORDER BY total_pledge" 
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
